b24a05b48e5d887d1a31db313553600a9378ce4b
Karim Wael <devkarim@hotmail.com>
2023-08-28 21:51:35 +0300

    feat: add more user utlity functions

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,6 +3,8 @@ import { hash } from "@/lib/hash";
 import { exclude } from "@/lib/exclude-prisma";
 import { CreateUser } from "@/schema/user.schema";
 
+import { User } from "@prisma/client";
+
 export const createUser = async (data: CreateUser) => {
   const hashedPassword = await hash(data.password);
   return prisma.user.create({
@@ -14,5 +16,16 @@ export const createUser = async (data: CreateUser) => {
 export const getFullUserByEmail = async (email: string) =>
   prisma.user.findUnique({ where: { email } });
 
+export const getFullUserById = async (id: number) =>
+  prisma.user.findUnique({ where: { id } });
+
 export const isEmailTaken = async (email: string) =>
   !!(await getFullUserByEmail(email));
+
+export const parseSession = (user: User): Express.User => {
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+  };
+};
