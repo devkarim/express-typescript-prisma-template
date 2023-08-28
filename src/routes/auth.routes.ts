dfa7eb6b4906bccb6a7d8239f96ce304fd83560f
Karim Wael <devkarim@hotmail.com>
2023-08-28 21:54:01 +0300

    feat: implement passport auth in /login api route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import passport from "passport";
 
 import authValidator from "@/validators/auth.validator";
+import requireAuth from "@/middlewares/auth/requireAuth";
 import authController from "@/controllers/auth.controller";
 
 const authRouter = express.Router();
@@ -8,11 +10,23 @@ const authRouter = express.Router();
 // @route     POST /auth/login
 // @desc      Log into user's account
 // @access    Public
-authRouter.post("/login", authValidator.login, authController.login);
+authRouter.post(
+  "/login",
+  authValidator.login,
+  passport.authenticate("local", {
+    failWithError: true,
+  }),
+  authController.login
+);
 
 // @route     POST /auth/register
 // @desc      Create a new account
 // @access    Public
 authRouter.post("/register", authValidator.register, authController.register);
+
+// @route     GET /auth/me
+// @desc      Fetches current session data
+// @access    Public
+authRouter.get("/me", requireAuth, authController.me);
 
 export default authRouter;
