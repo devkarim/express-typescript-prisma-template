afd0052781563ea9570a10c142f6da97a0445f82
Karim Wael <devkarim@hotmail.com>
2023-08-28 21:48:21 +0300

    feat: add api_domain constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,6 +3,7 @@ import { env } from "./env";
 export const isDevelopment = env.NODE_ENV === "development";
 export const isProduction = env.NODE_ENV === "production";
 
-export const APP_URL = isDevelopment
-  ? "http://localhost:8000"
-  : "https://api.example.com";
+export const API_DOMAIN = isDevelopment ? "localhost" : "api.example.com";
+export const API_URL = isDevelopment
+  ? `http://${API_DOMAIN}:8000`
+  : `https://${API_DOMAIN}`;
